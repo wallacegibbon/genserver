24f88aaa9c7c5c1bdb862c617a771a7e121c2a61
wallacegibbon <wallacegibbon@aliyun.com>
2017-11-23 15:29:12 +0800

    global error emit can be disabled now(disabled by default)

--- test/configured/a/playage.js
+++ test/configured/a/playage.js
@@ -1,8 +1,14 @@
 function makeYouOld(args) {
+  if (isNaN(parseInt(args.age)))
+    throw new Error("age should be number");
+
   return Object.assign({}, args, { age: Number(args.age) * 2 });
 }
 
 function makeYouYoung(args) {
+  if (isNaN(parseInt(args.age)))
+    throw new Error("age should be number");
+
   return Object.assign({}, args, { age: Number(args.age) / 2 });
 }
 
