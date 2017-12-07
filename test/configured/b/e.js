1995678f11433f272648afae9f983a5d22011e35
wallacegibbon <wallacegibbon@aliyun.com>
2017-12-07 16:21:42 +0800

    errorhandler changed, user controll the representation of error situation.

--- test/configured/b/e.js
+++ test/configured/b/e.js
@@ -1,8 +1,7 @@
 function errorHandler(e, ctx) {
   const i = `[${e.name}]<${ctx.query.cmd}>${e.message}`;
   ctx.logger.error(`[${ctx.clientIp}]${i}`);
-  //return 1; //Sometimes you need use number to represent error type.
-  return i;
+  return { error: i, code: 60001 };
 }
 
 
